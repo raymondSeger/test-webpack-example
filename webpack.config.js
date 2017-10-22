2fa767c5a0ed42befd861d14b3ca0ccec4c4e639
Raymond Seger <raymondseger@live.com>
2017-10-22 19:54:08 +0700

    Revert "Revert "add SCSS loader for webpack, use sass-loader and node-sass""
    This reverts commit 7ac9181c605827c347aeff04f340027c9f895e03.

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
                 test: /\.css/,
                 exclude: /node_modules/,
                 loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.scss/,
+                exclude: /node_modules/,
+                loader: 'style-loader!css-loader!sass-loader'
             }
         ]
     },
