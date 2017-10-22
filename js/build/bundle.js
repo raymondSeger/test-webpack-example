8453353201dae833bbcdb6c9d4b58c8a2b6669f8
Raymond Seger <raymondseger@live.com>
2017-10-22 19:05:01 +0700

    just use -d for sourcemaps, now you can debug the javascript
    just use -d for sourcemaps, now you can debug the javascript

--- js/build/bundle.js
+++ js/build/bundle.js
@@ -65,52 +65,51 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/*!***************************!*\
+  !*** multi ./utils ./app ***!
+  \***************************/
+/*! dynamic exports provided */
+/*! all exports used */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(1);
-module.exports = __webpack_require__(2);
+__webpack_require__(/*! ./utils */1);
+module.exports = __webpack_require__(/*! ./app */2);
 
 
 /***/ }),
 /* 1 */
+/*!******************!*\
+  !*** ./utils.js ***!
+  \******************/
+/*! dynamic exports provided */
+/*! all exports used */
 /***/ (function(module, exports) {
 
-console.log("this is utils file");
+eval("console.log(\"this is utils file\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMS5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3V0aWxzLmpzP2U4ZmIiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJ0aGlzIGlzIHV0aWxzIGZpbGVcIik7XG5cblxuLy8vLy8vLy8vLy8vLy8vLy8vXG4vLyBXRUJQQUNLIEZPT1RFUlxuLy8gLi91dGlscy5qc1xuLy8gbW9kdWxlIGlkID0gMVxuLy8gbW9kdWxlIGNodW5rcyA9IDAiXSwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///1\n");
 
 /***/ }),
 /* 2 */
+/*!*****************!*\
+  !*** ./app.es6 ***!
+  \*****************/
+/*! dynamic exports provided */
+/*! all exports used */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-//require('./login');
-
-
-var _login = __webpack_require__(3);
-
-(0, _login.login)('admin', 'idunno');
-
-document.write("Welcome to Big Hair Concert!");
-
-console.log('App Loaded');
+eval("//require('./login');\n\n\nvar _login = __webpack_require__(/*! ./login */ 3);\n\n(0, _login.login)('admin', 'idunno');\n\ndocument.write(\"Welcome to Big Hair Concert!\");\n\nconsole.log('App Loaded');//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMi5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2FwcC5lczY/OWUzZSJdLCJzb3VyY2VzQ29udGVudCI6WyIvL3JlcXVpcmUoJy4vbG9naW4nKTtcclxuaW1wb3J0IHtsb2dpbn0gZnJvbSBcIi4vbG9naW5cIlxyXG5cclxubG9naW4oJ2FkbWluJywgJ2lkdW5ubycpO1xyXG5cclxuZG9jdW1lbnQud3JpdGUoXCJXZWxjb21lIHRvIEJpZyBIYWlyIENvbmNlcnQhXCIpO1xyXG5cclxuY29uc29sZS5sb2coJ0FwcCBMb2FkZWQnKTtcblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9hcHAuZXM2Il0sIm1hcHBpbmdzIjoiOzs7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///2\n");
 
 /***/ }),
 /* 3 */
+/*!*******************!*\
+  !*** ./login.es6 ***!
+  \*******************/
+/*! dynamic exports provided */
+/*! all exports used */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-Object.defineProperty(exports, '__esModule', {
-    value: true
-});
-var login = function login(username, password) {
-    if (username !== 'admin' || password !== 'radical') {
-        console.log('incorrect login');
-    }
-};
-
-// login('admin', 'idunno');
-exports.login = login;
+eval("\n\nObject.defineProperty(exports, '__esModule', {\n    value: true\n});\nvar login = function login(username, password) {\n    if (username !== 'admin' || password !== 'radical') {\n        console.log('incorrect login');\n    }\n};\n\n// login('admin', 'idunno');\nexports.login = login;//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2xvZ2luLmVzNj9hMTY0Il0sInNvdXJjZXNDb250ZW50IjpbImxldCBsb2dpbiA9ICh1c2VybmFtZSwgcGFzc3dvcmQpID0+IHtcclxuICAgIGlmKHVzZXJuYW1lICE9PSAnYWRtaW4nIHx8IHBhc3N3b3JkICE9PSAncmFkaWNhbCcpIHtcclxuICAgICAgICBjb25zb2xlLmxvZygnaW5jb3JyZWN0IGxvZ2luJyk7XHJcbiAgICB9XHJcbn07XHJcblxyXG4vLyBsb2dpbignYWRtaW4nLCAnaWR1bm5vJyk7XHJcbmV4cG9ydCB7bG9naW59XG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIC4vbG9naW4uZXM2Il0sIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTs7QUFFQSIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///3\n");
 
 /***/ })
 /******/ ]);
